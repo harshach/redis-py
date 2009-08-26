ea00cce3394798bd152cdc069c99269bb9bc4f24
Harsha <chintala@softsupply-lm.corp.yahoo.com>
2009-08-26 13:32:46 -0700

    fixes

--- lib/hash_ring.py
+++ lib/hash_ring.py
@@ -1,10 +1,11 @@
+
 from __future__ import generators
 import zlib
 
-POINTS_PER_SERVER=160
+POINTS_PER_SERVER=5
 
 def binary_search(ary,value):
-    upper=len(ary)
+    upper=len(ary) - 1
     lower=0
     idx=0
     while (lower <= upper):
@@ -25,35 +26,34 @@ class HashRing(object):
         self.nodes = []
         self.sorted_keys = []
         for node in nodes:
-            add_node(node)
+            self.add_node(node)
     
 
     def add_node(self,node):
         self.nodes.append(node)
-        for i in range(self.repicas):
+        for i in xrange(0,self.replicas):
             key = zlib.crc32("%s:%d" % (node,i))
             self.ring[key] = node
             self.sorted_keys.append(key)
-        end
         self.sorted_keys.sort()
 
-    def remove_node(node):
-        for i in range(self.replicas):
+    def remove_node(self,node):
+        for i in xrange(0,self.replicas):
             key = zlib.crc32("%s:%d" % (node,i))
             del self.ring[key]
             self.sorted_keys.remove(k)
     
-    def get_node(key):
-        get_node_pos(key)[0]
+    def get_node(self,key):
+        return self.get_node_pos(key)[0]
     
-    def get_node_pos(key):
-        if self.ring.size == 0:
+    def get_node_pos(self,key):
+        if not self.ring:
             return [None,None]
         crc = zlib.crc32(key)
         idx = binary_search(self.sorted_keys,crc)
         return [self.ring[self.sorted_keys[idx]],idx]
 
-    def iter_nodes(key):
+    def iter_nodes(self,key):
         if self.ring.size == 0:
             yield [None,None]
         pos = get_node_post(key)[1]
